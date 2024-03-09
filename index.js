91c64562bf05b30e770f440d4a28c969fc5d8fa1
Ahmed Chouihi <ahmedchouihi@gmail.com>
2024-03-09 23:36:17 +0100

    Text-to-embedding

--- index.js
+++ index.js
@@ -8,26 +8,11 @@ const genAI = new GoogleGenerativeAI(process.env.API_KEY)
 
 
 async function run(){
-    const model = genAI.getGenerativeModel({model: "gemini-pro"})
-    const chat=model.startChat({
-        history:[
-            {
-                role:"user",
-                parts:"Hello , I have 2 cats in my house"
-            },
-            {
-                role:"model",
-                parts: "Great to meet you. What would you like to know"
-            }
-        ]
-    })
-
-    const msg= "How many paws are in my house ?"
-
-    const result=await chat.sendMessage(msg)
-    const response=await result.response
-    const text=response.text()
-    console.log(text)
+    const model = genAI.getGenerativeModel({model: "embedding-001"})
+    const text="The quick brown fox jumped over the lazy do"
+    const result=await model.embedContent(text);
+    const embedding=result.embedding
+    console.log(embedding)
 }
 
 run()
